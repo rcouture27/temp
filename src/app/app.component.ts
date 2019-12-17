07bb44c274a8ef07feab3eb5394398eadecdd025
vagrant <vagrant@ubuntu1804.localdomain>
2019-12-17 15:03:25 +0000

    full crud functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,16 +8,20 @@ import { HttpClient } from '@angular/common/http';
 })
 export class AppComponent implements OnInit {
   loadedPosts = [];
+  title = "";
+  content = "";
 
   constructor(private http: HttpClient) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.fetchPosts();
+  }
 
   onCreatePost(postData: { title: string; content: string }) {
     // Send Http request
     this.http
       .post(
-        'http://localhost:2202/api/Users',
+      'http://localhost:3000/api/Angulars',
         postData
       )
       .subscribe(responseData => {
@@ -27,9 +31,24 @@ export class AppComponent implements OnInit {
 
   onFetchPosts() {
     // Send Http request
+    this.fetchPosts(); 
+   }
+
+  onClearPosts(id: number) {
+    // Send Http request
+    this.http.delete('http://localhost:3000/api/Angulars/' + id).subscribe();
+  }
+  
+  private fetchPosts() {
+    this.http.get('http://0.0.0.0:3000/api/Angulars')
+    .subscribe(posts => {
+    console.log(posts);
+    });
   }
 
-  onClearPosts() {
-    // Send Http request
+  onUpdatePosts(postData: { title: string; content: string; id: number}) {
+    this.http.put('http://0.0.0.0:3000/api/Angulars', postData).subscribe(posts => {
+      console.log(posts);
+    });
   }
 }
